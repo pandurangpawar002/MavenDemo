e0e7c926b6fc1ae2353d15dd420c48771895f426
pandurangpawar002 <pandurangpawar002@gmail.com>
2021-12-26 23:49:31 +0530

    Hello Maven project uploading to git

---
+++ src/test/java/testpack/DemoMaven.java
@@ -0,0 +1,30 @@
+
+package testpack;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoMaven {
+
+	@Test
+	public static void startapp() {
+		WebDriver driver;//
+
+		System.setProperty("webdriver.chrome.driver",
+				"D:\\Atomation\\Selenium jars and drivers\\Chrome 96\\chromedriver.exe");
+
+		driver = new ChromeDriver();
+		driver.get("https://www.guru99.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		String currenturl = driver.getCurrentUrl();
+		Assert.assertTrue(currenturl.contains("guru99.com"));
+		Reporter.log("Browser is opened");
+		driver.manage().window().maximize();
+	}
+}
